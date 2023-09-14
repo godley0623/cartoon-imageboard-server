ffe8d8caca4466ed397055a8b7e800f55bf450a2
godley0623 <godley11691@yahoo.com>
2023-09-13 22:12:21 -0400

    added delete route for threads

---
+++ src/cloudinary/cloudinaryConfig.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv'
+import cloudinary from 'cloudinary'
+import { response } from 'express';
+
+dotenv.config()
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET,
+    secure: true 
+})
+
+export function deleteThreadContent(folder) {
+
+    function deleteFolder(folder) {
+        cloudinary.v2.api.delete_folder(folder)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    function deleteAllImagesInFolder(folder) {
+        cloudinary.api
+            .delete_resources_by_prefix(`${folder}/`, {type: 'authenticated'})
+            .then(result => {
+                console.log(result)
+                deleteFolder(folder)
+            })
+    }
+
+    deleteAllImagesInFolder(folder)
+
+}
--- src/routes/threads.js
+++ src/routes/threads.js
@@ -1,15 +1,16 @@
 import express from "express"
 import Thread from "../models/thread.js"
+import { deleteThreadContent } from "../cloudinary/cloudinaryConfig.js"
 
 export const router = express.Router()
 
 router.get("/", (req, res) => {
     Thread.find({})
     .then((response) => {
-        res.status(200).send(response);
+        res.status(200).send(response)
     })
     .catch((err) => {
-        res.status(404).send({status: 404, error: err});
+        res.status(404).send({status: 404, error: err})
     })
 })
 
@@ -17,10 +18,10 @@ router.get("/:id", (req, res) => {
     const id = Number(req.params.id)
     Thread.findOne({ postNumber: id })
     .then((response) => {
-        res.status(200).send(response);
+        res.status(200).send(response)
     })
     .catch((err) => {
-        res.status(404).send({status: 404, error: err});
+        res.status(404).send({status: 404, error: err})
     })
 })
 
@@ -28,20 +29,33 @@ router.put("/reply/:id", (req, res) => {
     const id = req.params.id
     Thread.findOneAndUpdate( { postNumber: id }, { replies: req.body } )
     .then(() => {
-        res.status(200).send('Replies were updated');
+        res.status(200).send('Replies were updated')
     })
     .catch((err) => {
-        res.status(404).send({status: 404, error: err});
+        res.status(404).send({status: 404, error: err})
     })
 })
 
 router.post("/", (req, res) => {
     Thread.create(req.body)
     .then(() => {
-        res.status(200).send('Thread was created');
+        res.status(200).send('Thread was created')
     })
     .catch((err) => {
-        res.status(404).send({status: 404, error: err});
+        res.status(404).send({status: 404, error: err})
+    })
+})
+
+router.delete("/:id", (req, res) => {
+    const postNumber = Number(req.params.id)
+    deleteThreadContent(postNumber)
+
+    Thread.findOneAndDelete( { postNumber: postNumber } )
+    .then(() => {
+        res.status(200).send('Thread was deleted')
+    })
+    .catch((err) => {
+        res.status(404).send({status: 404, error: err})
     })
 })
 
